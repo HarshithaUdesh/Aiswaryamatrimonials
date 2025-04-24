e84b84bad6e47ab4ac53853a7119e143a5042430
HarshithaUdesh <harshithau219@gmail.com>
2025-04-24 21:56:26 +0530

    auth guard changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { AuthGuard } from './guard/auth.guard';
 const routes: Routes = [
   
   {
@@ -17,14 +17,6 @@ const routes: Routes = [
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   },
   {
-    path: 'tabs',
-    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule),
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: 'forgotpass',
     loadChildren: () => import('./pages/forgotpass/forgotpass.module').then( m => m.ForgotpassPageModule)
   },
@@ -32,6 +24,15 @@ const routes: Routes = [
     path: 'verifyotp',
     loadChildren: () => import('./pages/verifyotp/verifyotp.module').then( m => m.VerifyotpPageModule)
   },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule),canActivate: [AuthGuard],
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),canActivate: [AuthGuard],
+  },
+  
   {
     path: 'changepassword',
     loadChildren: () => import('./pages/changepassword/changepassword.module').then( m => m.ChangepasswordPageModule)
@@ -42,14 +43,15 @@ const routes: Routes = [
   },
   {
     path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule),canActivate: [AuthGuard],
   },
   {
     path: 'subscription',
-    loadChildren: () => import('./pages/subscription/subscription.module').then( m => m.SubscriptionPageModule)
-  },  {
+    loadChildren: () => import('./pages/subscription/subscription.module').then( m => m.SubscriptionPageModule),canActivate: [AuthGuard],
+  },
+  {
     path: 'contactus',
-    loadChildren: () => import('./pages/contactus/contactus.module').then( m => m.ContactusPageModule)
+    loadChildren: () => import('./pages/contactus/contactus.module').then( m => m.ContactusPageModule),canActivate: [AuthGuard],
   },
   {
     path: 'privacy',
@@ -65,7 +67,11 @@ const routes: Routes = [
   },
   {
     path: 'subscriptionhistory',
-    loadChildren: () => import('./pages/subscriptionhistory/subscriptionhistory.module').then( m => m.SubscriptionhistoryPageModule)
+    loadChildren: () => import('./pages/subscriptionhistory/subscriptionhistory.module').then( m => m.SubscriptionhistoryPageModule),canActivate: [AuthGuard],
+  },
+  {
+    path: 'togoprofiledetails',
+    loadChildren: () => import('./pages/togoprofiledetails/togoprofiledetails.module').then( m => m.TogoprofiledetailsPageModule),canActivate: [AuthGuard],
   }
 
 ];
