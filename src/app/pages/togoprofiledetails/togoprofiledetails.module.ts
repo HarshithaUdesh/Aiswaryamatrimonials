c09492a52e258e7d13fa92fa4af3dbe324800004
HarshithaUdesh <harshithau219@gmail.com>
2025-04-24 21:35:36 +0530

    filetr page  changes

--- src/app/pages/togoprofiledetails/togoprofiledetails.module.ts
+++ src/app/pages/togoprofiledetails/togoprofiledetails.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -15,6 +15,7 @@ import { TogoprofiledetailsPage } from './togoprofiledetails.page';
     IonicModule,
     TogoprofiledetailsPageRoutingModule
   ],
-  declarations: [TogoprofiledetailsPage]
+  declarations: [TogoprofiledetailsPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class TogoprofiledetailsPageModule {}
